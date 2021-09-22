b708702e57c097745a1a841c440a5883a90e30dd
AntonP <anton91popov@gmail.com>
2021-09-22 17:10:31 +0300

    Updated auto export

--- product/pipelines.py
+++ product/pipelines.py
@@ -6,8 +6,18 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import json
 
 class ProductPipeline:
-    def process_item(self, item, spider):
+    def process_item(self, item, spider):  # default method
+        # calling dumps to create json data.
+        line = json.dumps(dict(item)) + "\n"
+        # converting item to dict above, since dumps only intakes dict.
+        self.file.write(line)                    # writing content in output file.
         return item
+  
+    def open_spider(self, spider):
+        self.file = open('productdata.json', 'w')
+  
+    def close_spider(self, spider):
+        self.file.close()
